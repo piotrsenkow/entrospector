7d86e02fedd0bf4422ddedda30171561d7c06bd7
Piotr Senkow <piotrsenkow@gmail.com>
2017-10-12 13:15:30 -0500

    Added pauses between functions. Added a condition for main menu. Still need to fix program breaking when string inputed in menu

--- Entropy.py
+++ Entropy.py
@@ -10,27 +10,32 @@ def Quit():                            #Kill program >:)
 def PrintNetwork(): #prints all the genes and what they connect to
     for x in d:  # for each value in dictionary
         print("{} : {}".format(x, d[x]))  # prints key and all values attached to it
+    raw_input("press enter to return to menu")
     Menu()
 
 def PrintSet(): #print an individual key
     key = input("Which gene would you like to see all of its connections to?\n")
     key = str(key)
     print(d.get(key))
+    raw_input("press enter to return to menu")
     Menu()
 
 def DlenPrint(): #print length of the dictionary
-    print(len(d))
+    print("There are {} nodes".format(len(d)))
+    raw_input("press enter to return to menu")
     Menu()
 
 def PrintSystemsCounts():
     for x in counts:  # for loop that prints out the gene and how many genes it connects to, iterating through the "counts" dictionary
         print("Gene {} connects to {} genes".format(x, counts[x]))
+    raw_input("press enter to return to menu")
     Menu()
 
 def PrintSetsCounts():
     key=input("Which gene would you like to see the number of connections to?\n")
     key = str(key)
     print(counts.get(key))
+    raw_input("press enter to return to menu")
     Menu()
 
 def PrintGeneInformation():
@@ -38,6 +43,7 @@ def PrintGeneInformation():
     key = str(key)
     print("{} has {} genes connect to it".format(key, counts.get(key)))
     print("{} has the following genes connect to it: {}".format(key, d.get(key)))
+    raw_input("press enter to return to menu")
     Menu()
 
 def PrintIndividualEntropy():
@@ -46,6 +52,7 @@ def PrintIndividualEntropy():
     entropy = counts.get(key)  # code segment for calculating entropy value of that gene
     entropy = 1 / float(entropy)  # float type necessary unless you want python to tell you 0 entropy calculated
     print("The entropy of gene {} is {} because {} genes connect to it".format(key, entropy, counts.get(key)))
+    raw_input("press enter to return to menu")
     Menu()
 
 def PrintSystemEntropy():
@@ -56,13 +63,23 @@ def PrintSystemEntropy():
     print("1/{}".format(totalentropy))  # test to see what denominator is
     totalentropy = 1 / float(totalentropy)  # total entropy calculation
     print("The total entropy of the entire system is {}".format(totalentropy))
+    raw_input("press any key to return to menu")
     Menu()
 
 def Menu():            #Main Menu
     menu = {1: PrintNetwork, 2: PrintSet, 3: DlenPrint, 4: PrintSystemsCounts, 5: PrintSetsCounts,
             6: PrintGeneInformation, 7: PrintIndividualEntropy, 8: PrintSystemEntropy, 0: Quit}
+    f=open("options.txt")
+    for line in f:
+        print(line)
+    f.close()
     num = input()
-    menu[num]()
+    if 0<=num<=8:
+        menu[num]()
+    else:
+        raw_input("Please choose an option between 0 and 8\nPress enter to continue")
+        Menu()
+
 
 def main():
     with open("Yeast.txt") as f:  # while the text is open
